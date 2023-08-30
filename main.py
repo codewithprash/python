57614217e1f916c31d0825b7e439b9a23bf15af4
prash <87913082+codewithprash@users.noreply.github.com>
2023-08-31 01:07:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-from flask import Flask, request, send_file
-import qrcode
-from io import BytesIO
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/generate_qr', methods=['GET'])
-def generate_qr():
-    data = request.args.get('data', '')
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
-
-    img_io = BytesIO()
-    img.save(img_io, 'PNG')
-    img_io.seek(0)
-
-    return send_file(img_io, mimetype='image/png')
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run()
