10e9f945487934c094cbd635128dcb57f1b2a1ca
prash <87913082+codewithprash@users.noreply.github.com>
2023-09-01 00:07:33 +0530

    update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import base64
+import io
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
 import qrcode
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-QR_CODE_DIR = "https://universel-qr.onrender.com/"
+QR_CODE_DIR = ""
 
 origins = [
     "*",
@@ -22,7 +25,8 @@ app.add_middleware(
 async def read_root():
     return {"message": "Welcome to the QR Code Generator"}
 
-@app.get("/png/{data}")
+
+@app.get("/qr/{data}")
 async def generate_qr_code(data: str):
     if not data:
         raise HTTPException(status_code=400, detail="Data parameter is required")
@@ -39,6 +43,33 @@ async def generate_qr_code(data: str):
     img = qr.make_image(fill_color="black", back_color="white")
 
     # Save the image as a PNG file
-    file_path = f"{QR_CODE_DIR}{data}.png"
-    img.save(f"{data}.png")
-    return {"image_path": file_path}
+    file_path = f"{data}.png"
+    img.save(file_path)
+
+    
+
+    return FileResponse(file_path)
+
+@app.get("/code/{data}")
+async def generate_qr_code(data: str):
+    if not data:
+        raise HTTPException(status_code=400, detail="Data parameter is required")
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Convert the image to base64
+    img_io = io.BytesIO()
+    img.save(img_io, format="PNG")
+    img_io.seek(0)
+    img_base64 = base64.b64encode(img_io.read()).decode()
+
+    return {"image": img_base64}
